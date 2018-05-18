f4c34493b583cae4de9efecf6c34972ef4d317a8
vyudin <vyudin@tsconsulting.ru>
2018-05-18 18:40:01 +0300

    changed a little bit

--- src/main/java/AppleSortStreams.java
+++ src/main/java/AppleSortStreams.java
@@ -7,7 +7,7 @@ public class AppleSortStreams {
     public static void main(String[] args) {
         // Creating apples
         List<Apple> appleList = new ArrayList<>();
-        for (int i = 1; i < 50; i++) {
+        for (int i = 1; i < 50000; i++) {
             Apple apple = new Apple(i, (int) (Math.random()*230 + 1));
             if (i % 2 == 0) apple.setColor("green");
             appleList.add(apple);
@@ -17,21 +17,21 @@ public class AppleSortStreams {
         System.out.println("Sequential processing");
         long start = System.currentTimeMillis();
         List<Apple> heavyApples =
-                appleList.stream().filter((Apple a) -> a.getWeight() > 220)
+                appleList.stream().filter((Apple a) -> a.getWeight() > 229)
                                   .collect(toList());
         long finish = System.currentTimeMillis();
         System.out.println("It takes, ms: " + (finish - start));
-        for (Apple apple : heavyApples) System.out.println(apple);
+        heavyApples.forEach(System.out::println);
 
         // Parallel processing
         System.out.println("Parallel processing");
         start = System.currentTimeMillis();
         List<Apple> heavyApplesParallel =
-                appleList.parallelStream().filter((Apple a) -> a.getWeight() > 220)
+                appleList.parallelStream().filter((Apple a) -> a.getWeight() > 229)
                                           .collect(toList());
         finish = System.currentTimeMillis();
         System.out.println("It takes, ms: " + (finish - start));
-        for (Apple apple : heavyApplesParallel) System.out.println(apple);
+        heavyApplesParallel.forEach(System.out::println);
 
     }
 }
