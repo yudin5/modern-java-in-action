7a35682e267c54ca13f505cd323464cedcbb9873
yudin5 <yudin5@yandex.ru>
2022-01-07 13:54:58 +0300

    add chapter 3

---
+++ src/main/java/part_1_basics/chapter_3/RunnableExamples.java
@@ -0,0 +1,20 @@
+package part_1_basics.chapter_3;
+
+public class RunnableExamples {
+    public static void main(String[] args) {
+        Runnable r1 = () -> System.out.println("Hello world 1");
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world 2");
+            }
+        };
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("Hello world 3"));
+    }
+
+    public static void process(Runnable r) {
+        r.run();
+    }
+}
