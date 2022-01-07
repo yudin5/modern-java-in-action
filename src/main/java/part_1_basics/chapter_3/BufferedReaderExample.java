24d6cddf893c84436f9893210bc91dd094170454
yudin5 <yudin5@yandex.ru>
2022-01-07 18:45:04 +0300

    add BufferedReaderExample

---
+++ src/main/java/part_1_basics/chapter_3/BufferedReaderExample.java
@@ -0,0 +1,20 @@
+package part_1_basics.chapter_3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderExample {
+
+    public static void main(String[] args) throws IOException {
+        String result = processFile((BufferedReader br) -> br.readLine() + ", " + br.readLine());
+        System.out.println("result = " + result);
+    }
+
+    public static String processFile(BufferedReaderProcessor processor) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            return processor.process(br);
+        }
+    }
+}
+
