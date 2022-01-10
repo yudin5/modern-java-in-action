ae7ba3c9605a9f4ffbb2978ab206a0a5d2301bff
yudin5 <yudin5@yandex.ru>
2022-01-10 22:10:52 +0300

    add GroupingByExample

---
+++ src/main/java/part_1_basics/chapter_4_streams/GroupingByExample.java
@@ -0,0 +1,18 @@
+package part_1_basics.chapter_4_streams;
+
+import part_1_basics.chapter_4_streams.model.Dish;
+import part_1_basics.chapter_4_streams.model.MenuUtils;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class GroupingByExample {
+    public static void main(String[] args) {
+        List<Dish> menu = MenuUtils.getMenu();
+        Map<Dish.Type, List<Dish>> dishesByType = menu.stream()
+                .collect(groupingBy(Dish::getType));
+        System.out.println("dishesByType = " + dishesByType);
+    }
+}
