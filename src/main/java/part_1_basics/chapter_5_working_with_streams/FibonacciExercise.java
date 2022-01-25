13852a609729ecf4d283d8560d56d43ca611a559
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2022-01-25 12:15:47 +0300

    add IterateExample and FibonacciExercise

---
+++ src/main/java/part_1_basics/chapter_5_working_with_streams/FibonacciExercise.java
@@ -0,0 +1,15 @@
+package part_1_basics.chapter_5_working_with_streams;
+
+import java.util.stream.Stream;
+
+// Задача: сгенерировать первые 20 элементов последовательности пар Фибоначчи с помощью метода iterate
+// (0, 1), (1, 1), (1, 2), (2, 3), (3, 5), (5, 8) и т.д.
+public class FibonacciExercise {
+    public static void main(String[] args) {
+
+        Stream.iterate(new int[]{0, 1},
+                        arr -> new int[]{arr[1], arr[0] + arr[1]})
+                .limit(20)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ")"));
+    }
+}
