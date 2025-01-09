a9051f5a2c4be845e655dda70e4a7db90e1f82fa
yudin5 <yudin5@yandex.ru>
2025-01-09 21:49:41 +0300

    small refactoring

--- src/main/java/chapter_1/AppleSortStreams.java
+++ src/main/java/chapter_1/AppleSortStreams.java
@@ -11,9 +11,11 @@ public class AppleSortStreams {
     public static void main(String[] args) {
         // Creating apples
         List<Apple> appleList = new ArrayList<>();
-        for (int i = 1; i < 50000; i++) {
-            Apple apple = new Apple(i, (int) (Math.random()*230 + 1));
-            if (i % 2 == 0) apple.setColor("green");
+        for (int i = 1; i < 50_000_000; i++) {
+            Apple apple = new Apple(i, (int) (Math.random() * 230 + 1));
+            if (i % 2 == 0) {
+                apple.setColor("green");
+            }
             appleList.add(apple);
         }
 
@@ -21,21 +23,22 @@ public class AppleSortStreams {
         System.out.println("Sequential processing");
         long start = System.currentTimeMillis();
         List<Apple> heavyApples =
-                appleList.stream().filter((Apple a) -> a.getWeight() > 229)
-                                  .collect(toList());
+                appleList.stream()
+                        .filter((Apple a) -> a.getWeight() > 229)
+                        .collect(toList());
         long finish = System.currentTimeMillis();
         System.out.println("It takes, ms: " + (finish - start));
-        heavyApples.forEach(System.out::println);
+//        heavyApples.forEach(System.out::println);
 
         // Parallel processing
         System.out.println("Parallel processing");
         start = System.currentTimeMillis();
         List<Apple> heavyApplesParallel =
                 appleList.parallelStream().filter((Apple a) -> a.getWeight() > 229)
-                                          .collect(toList());
+                        .collect(toList());
         finish = System.currentTimeMillis();
         System.out.println("It takes, ms: " + (finish - start));
-        heavyApplesParallel.forEach(System.out::println);
+//        heavyApplesParallel.forEach(System.out::println);
 
     }
 }
